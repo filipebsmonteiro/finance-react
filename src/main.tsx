b12722126acc1ce9ae095f6f2a68565e994b70dd
Filipe Monteiro <filipebsmonteiro@gmail.com>
2024-07-22 17:46:00 -0300

    Comment Strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
@@ -9,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AppContext.Provider value={data}>
       <BrowserRouter>
         <Provider store={store}>
@@ -17,5 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </Provider>
       </BrowserRouter>
     </AppContext.Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 )
